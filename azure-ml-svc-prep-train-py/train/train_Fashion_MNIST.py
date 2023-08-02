1ce224f9a257d8ea443dbce27def1511b3dc7515
MagicMotion <MagicMotion2003@proton.me>
2023-08-02 17:40:29 +0000

    removing readme

---
+++ azure-ml-svc-prep-train-py/train/train_Fashion_MNIST.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+import warnings
+warnings.filterwarnings('ignore')
+
+import os
+import numpy as np
+from functools import partial
+import matplotlib.pyplot as pl
