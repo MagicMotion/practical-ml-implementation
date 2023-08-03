352e1362d22245a4a6bf4db1c30359dfb184ce17
MagicMotion <MagicMotion2003@proton.me>
2023-08-03 21:55:16 +0000

    More TODO

--- azure-ml-svc-prep-train-py/train/train_Fashion_MNIST.py
+++ azure-ml-svc-prep-train-py/train/train_Fashion_MNIST.py
@@ -12,4 +12,12 @@ from tensorflow.keras.datasets import fashion_mnist
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
-from tensorflow.
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras import backend as K
+
+import azureml.core
+from azureml.core.run import Run
+
+print("TensorFlow version:", tf.__version__)
+print("Using GPU build:", tf.test.is_built_with_cuda())
+print("Is GPU available:", tf.test.is_gpu_
