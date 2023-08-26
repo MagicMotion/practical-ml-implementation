974ef43365491c40e9ea85fbaf46a900cc9ebe1c
MagicMotion <MagicMotion2003@proton.me>
2023-08-26 15:17:16 +0000

    Bump to 2.0.5

--- deploy-r-models/R/00-train-model.R
+++ deploy-r-models/R/00-train-model.R
@@ -1,1 +1,6 @@
-#################
+##########################################################
+#   Create, Test and Save a Logistic Regression Model    #
+##########################################################
+
+# Use logistic regression equation of vehicle transmission 
+# in the data set mtcars to estimate the probability of
