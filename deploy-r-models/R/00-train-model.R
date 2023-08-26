fce0198e6a8d8c1c26586151fe426d98569b3fb6
MagicMotion <MagicMotion2003@proton.me>
2023-08-26 19:25:15 +0000

    Syntax

--- deploy-r-models/R/00-train-model.R
+++ deploy-r-models/R/00-train-model.R
@@ -3,4 +3,14 @@
 ##########################################################
 
 # Use logistic regression equation of vehicle transmission 
-# in the data set mtcars to estimate the probability of
+# in the data set mtcars to estimate the probability of 
+# a vehicle being fitted with a manual transmission 
+# based on horsepower (hp) and weight (wt)
+
+?mtcars
+summary(mtcars)
+
+# Create glm model with `mtcars` dataset
+carsModel <- glm(formula = am ~ hp + wt, data = mtcars, family = binomial)
+
+# Produce a predicti
