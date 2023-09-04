499d4d5cc0b8eff2369ff95743d9af1427dff7ae
MagicMotion <MagicMotion2003@proton.me>
2023-09-04 15:00:29 +0000

    revised readme

--- deploy-r-models/R/02b-deploy-aci.sh
+++ deploy-r-models/R/02b-deploy-aci.sh
@@ -6,4 +6,13 @@ IFS=$'\n\t'
 # -o: prevents errors in a pipeline from being masked
 # IFS new value is less likely to cause confusing bugs when looping arrays or arguments (e.g. $@)
 
-usage() { echo "Usage: $0 -i <subscriptionId> -g <resourceGroupName> -n <deploymentName> -l <resourceGroupLocation>" 1
+usage() { echo "Usage: $0 -i <subscriptionId> -g <resourceGroupName> -n <deploymentName> -l <resourceGroupLocation>" 1>&2; exit 1; }
+
+declare subscriptionId=""
+declare resourceGroupName=""
+declare deploymentName="car-svc-aci-`date '+%Y-%m-%d-%H-%M-%S'`"
+declare resourceGroupLocation=""
+declare aciDnsNameLabel=""
+declare acrName=""
+
+# Initialize parameters sp
