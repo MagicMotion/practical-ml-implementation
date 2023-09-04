2962037896f81fe3fee0ddcde0596ad413d37298
MagicMotion <MagicMotion2003@proton.me>
2023-09-04 07:56:04 +0000

    code reuse

--- deploy-r-models/R/02b-deploy-aci.sh
+++ deploy-r-models/R/02b-deploy-aci.sh
@@ -3,4 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # -e: immediately exit if any command has a non-zero exit status
-# -o: prevents errors in a pipeline f
+# -o: prevents errors in a pipeline from being masked
+# IFS new value is less likely to cause confusing bugs when looping arrays or arguments (e.g. $@)
+
+usage() { echo "Usage: $0 -i <subscriptionId> -g <resourceGroupName> -n <deploymentName> -l <resourceGroupLocation>" 1
