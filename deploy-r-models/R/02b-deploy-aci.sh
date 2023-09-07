7f9fb4a0daa0fa5f24bbf6767d8c0a5b07e71d7f
MagicMotion <MagicMotion2003@proton.me>
2023-09-07 12:16:06 +0000

    Prepare 0.9.2 release

--- deploy-r-models/R/02b-deploy-aci.sh
+++ deploy-r-models/R/02b-deploy-aci.sh
@@ -57,4 +57,13 @@ if [[ -z "$resourceGroupName" ]]; then
 fi
 
 if [[ -z "$deploymentName" ]]; then
-	echo "Enter a name for this deploy
+	echo "Enter a name for this deployment:"
+	read deploymentName
+fi
+
+if [[ -z "$resourceGroupLocation" ]]; then
+	echo "If creating a *new* resource group, you need to set a location "
+	echo "You can lookup locations with the CLI using: az account list-locations "
+	
+	echo "Enter resource group location:"
+	re
