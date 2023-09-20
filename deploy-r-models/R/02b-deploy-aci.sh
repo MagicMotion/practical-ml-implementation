b8eb3ea1bd201682df876383dfa0d199f06ce32c
MagicMotion <MagicMotion2003@proton.me>
2023-09-20 09:38:21 +0000

    3.0.1 release

--- deploy-r-models/R/02b-deploy-aci.sh
+++ deploy-r-models/R/02b-deploy-aci.sh
@@ -125,4 +125,9 @@ echo "Uploading Docker Image..."
 echo "Starting deployment..."
 (
 	acrPassword=$(az acr credential show -g "$resourceGroupName" -n "$acrName" | jq -r .passwords[0].value)
-	az container create -g "$resourceGro
+	az container create -g "$resourceGroupName" -n cars-svc-aci --image "$acrName.azurecr.io/carssvc:latest" --ports 8000 --dns-name-label "$aciDnsNameLabel" --registry-username "$acrName" --registry-password "$acrPassword"
+)
+
+if [ $?  == 0 ];
+ then
+	echo "Azure Container Instance has been successfully d
