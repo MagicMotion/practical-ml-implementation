f7fb61907b3d0f4dc845da974125c69a45380c35
MagicMotion <MagicMotion2003@proton.me>
2023-09-21 20:41:03 +0000

    Updated the version

--- deploy-r-models/deploy-mmls.sh
+++ deploy-r-models/deploy-mmls.sh
@@ -2,4 +2,8 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# -e: immediately exit
+# -e: immediately exit if any command has a non-zero exit status
+# -o: prevents errors in a pipeline from being masked
+# IFS new value is less likely to cause confusing bugs when looping arrays or arguments (e.g. $@)
+
+usage() { echo "Usage: $0 -i <subscriptionId> 
