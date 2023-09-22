df73a2641b0c9a51a15959e3725bee4880862630
MagicMotion <MagicMotion2003@proton.me>
2023-09-22 08:51:58 +0000

    ReadMe update

--- deploy-r-models/deploy-mmls.sh
+++ deploy-r-models/deploy-mmls.sh
@@ -6,4 +6,9 @@ IFS=$'\n\t'
 # -o: prevents errors in a pipeline from being masked
 # IFS new value is less likely to cause confusing bugs when looping arrays or arguments (e.g. $@)
 
-usage() { echo "Usage: $0 -i <subscriptionId> 
+usage() { echo "Usage: $0 -i <subscriptionId> -g <resourceGroupName> -n <deploymentName> -l <resourceGroupLocation> -v <vm-name> -u <admin-username> -p <admin-password>" 1>&2; exit 1; }
+
+declare subscriptionId=""
+declare resourceGroupName=""
+declare deploymentName="msftmlsvr-`date '+%Y-%m-%d-%H-%M-%S'`"
+declare r
