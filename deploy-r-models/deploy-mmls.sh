e5a98d93dc2c65922a6b1e2f8dc52315dd79d874
MagicMotion <MagicMotion2003@proton.me>
2023-09-30 20:37:18 +0000

    Ajout du fichier XML du pakage fr-FR.

--- deploy-r-models/deploy-mmls.sh
+++ deploy-r-models/deploy-mmls.sh
@@ -89,4 +89,17 @@ fi
 if [[ -z "$password" ]]; then
 	while : ; do
 		echo -n "Enter a password for the vm admin:"
-		read -s passwo
+		read -s password
+		echo
+		echo -n "Please repeat the password for the vm admin:"
+		read -s password_confirm
+		echo
+		if [[ "$password" == "$password_confirm" ]]; then
+			break
+		else
+			echo "The passwords do not match. Please retry."
+		fi
+	done
+fi
+
+if [ -z "$subscriptionId"
