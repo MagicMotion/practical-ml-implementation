fd168afce468cce0b8b8b9fd157877b9799b8ae3
MagicMotion <MagicMotion2003@proton.me>
2023-10-21 14:19:40 +0000

    Updated Code

--- deploy-r-models/setup/install-mmls-ubuntu.sh
+++ deploy-r-models/setup/install-mmls-ubuntu.sh
@@ -5,4 +5,16 @@ declare password=""
 while getopts ":p:" arg; do
 	case "${arg}" in
 		p)
-			password=${OPTARG
+			password=${OPTARG}
+			;;
+		esac
+done
+shift $((OPTIND-1))
+
+if [[ -z "$password" ]]; then
+	while : ; do
+		echo -n "Enter a password for the Machine Learning Server admin:"
+		read -s password
+		echo
+		echo -n "Please repeat the password for the Machine Learning Server admin:"
+		r
