4f93c88e65af011714582aeacdb9b6bf23d5c246
MagicMotion <MagicMotion2003@proton.me>
2023-10-21 22:42:51 +0000

    a

--- deploy-r-models/setup/install-mmls-ubuntu.sh
+++ deploy-r-models/setup/install-mmls-ubuntu.sh
@@ -17,4 +17,15 @@ if [[ -z "$password" ]]; then
 		read -s password
 		echo
 		echo -n "Please repeat the password for the Machine Learning Server admin:"
-		r
+		read -s password_confirm
+		echo
+		if [[ "$password" == "$password_confirm" ]]; then
+			break
+		else
+			echo "The passwords do not match. Please retry."
+		fi
+	done
+fi
+
+# Optionally, if your system does not have the https apt transport option
+apt-get install apt-transpor
