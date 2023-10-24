70eabe2493ef08d7231fc78d315499794d9d3b36
MagicMotion <MagicMotion2003@proton.me>
2023-10-24 09:28:03 +0000

    Bump the version number.

--- deploy-r-models/setup/install-mmls-ubuntu.sh
+++ deploy-r-models/setup/install-mmls-ubuntu.sh
@@ -40,4 +40,16 @@ echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO 
 wget https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
 
 # Register the repo
-dpkg -i packages-m
+dpkg -i packages-microsoft-prod.deb
+
+# Remove deb file
+rm -rf packages-microsoft-prod.deb
+
+# Add the Microsoft public signing key for Secure APT
+apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
+
+# Update packages on your system
+apt-get update
+
+# Install the server
+apt-ge
